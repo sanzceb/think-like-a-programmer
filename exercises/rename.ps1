38f93be72010f90b02b821b9ce5469a26744403e
Sergio Sanz <ssanz.ceb@outlook.com>
2024-12-22 18:12:28 +0100

    delete old folder

--- exercises/rename.ps1
+++
@@ -1,7 +0,0 @@
-# Get all chapter directories matching the pattern chapter0[0-9]
-$chapterDirs = Get-ChildItem -Directory | Where-Object { $_.Name -match '^chapter0\d$' }
-
-foreach ($chapter in $chapterDirs) {
-    $newName = $chapter.Name -replace '^chapter(0\d)$', 'ch$1'
-    Rename-Item -Path $chapter.FullName -NewName $newName -Verbose
-}
